b66050f35981bc2bcd46d2e0b51414d0335cc5dd
Dahbi-Dev <houssamdahbi007@gmail.com>
2024-06-09 20:14:28 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express")
+const cors = require("cors")
+const app = express();
+app.use(cors())
+
+app.get("/employes", (req, res) => {
+    const employes = [
+        {
+            "_id": "e1",
+            "nomEmp": "Lamrabet",
+            "prenomEmp": "Oussama",
+            "poste": "Directeur",
+            "departement": {
+                "codeDep": "1",
+                "nomDep": "RH"
+            }
+        },
+        {
+            "_id": "e2",
+            "nomEmp": "Bennani",
+            "prenomEmp": "Amina",
+            "poste": "Manager",
+            "departement": {
+                "codeDep": "2",
+                "nomDep": "Finance"
+            }
+        },
+        {
+            "_id": "e3",
+            "nomEmp": "El Amrani",
+            "prenomEmp": "Youssef",
+            "poste": "Analyste",
+            "departement": {
+                "codeDep": "3",
+                "nomDep": "Marketing"
+            }
+        },
+        {
+            "_id": "e4",
+            "nomEmp": "Cherkaoui",
+            "prenomEmp": "Fatima",
+            "poste": "Chef de projet",
+            "departement": {
+                "codeDep": "4",
+                "nomDep": "IT"
+            }
+        },
+        {
+            "_id": "e5",
+            "nomEmp": "El Idrissi",
+            "prenomEmp": "Khalid",
+            "poste": "Développeur",
+            "departement": {
+                "codeDep": "5",
+                "nomDep": "Développement"
+            }
+        }
+    ]
+    res.json(employes)
+})
+
+app.listen(8000, () => {
+    console.log("server Running on port 8000");
+})
